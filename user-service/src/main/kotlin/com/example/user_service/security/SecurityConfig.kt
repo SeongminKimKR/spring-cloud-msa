1277f4a5bcae3c844fd7562cbc6cc2cf9e82fc1c
SeongminKimKR <gvf156@gmail.com>
2025-03-19 02:51:06 +0900

    feat: h2 콘솔 화면 나오도록 frameOption 최적화 비활성화

--- user-service/src/main/kotlin/com/example/user_service/security/SecurityConfig.kt
+++ user-service/src/main/kotlin/com/example/user_service/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.example.user_service.security
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -10,6 +11,10 @@ class SecurityConfig {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain = http
         .csrf { it.disable() }
-        .authorizeHttpRequests { it.requestMatchers("/users/**", "/h2-console/**").permitAll() }
+        .authorizeHttpRequests {
+            it.requestMatchers("/users/**").permitAll()
+            it.requestMatchers(PathRequest.toH2Console()).permitAll()
+        }
+        .headers { it.frameOptions { it.disable() } }
         .build()
 }
