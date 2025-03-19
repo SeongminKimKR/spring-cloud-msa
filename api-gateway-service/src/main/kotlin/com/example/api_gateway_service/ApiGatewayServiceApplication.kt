f74666b9efe715b4dfcf87ed058fb26822c2a1c8
SeongminKimKR <gvf156@gmail.com>
2025-03-19 21:52:57 +0900

    feat: spring cloud gateway 추가

---
+++ api-gateway-service/src/main/kotlin/com/example/api_gateway_service/ApiGatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.api_gateway_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGatewayServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiGatewayServiceApplication>(*args)
+}
