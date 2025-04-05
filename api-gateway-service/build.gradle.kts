49b4187540a19756df7f28df46e6219e43b10c5c
SeongminKimKR <gvf156@gmail.com>
2025-04-05 19:18:35 +0900

    feat: 공통 설정 추가

--- api-gateway-service/build.gradle.kts
+++ api-gateway-service/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	//jwt
 	implementation ("io.jsonwebtoken:jjwt-api:0.12.6")
 	implementation ("io.jsonwebtoken:jjwt-impl:0.12.6")
