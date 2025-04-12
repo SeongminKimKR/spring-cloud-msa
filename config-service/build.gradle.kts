5c8ae00c007f2c761e8e7e47ca609b9407d9acc9
SeongminKimKR <gvf156@gmail.com>
2025-04-12 18:46:56 +0900

    chore: spring cloud bus, rabbitmq설정 추가 및 테스트를 위한 설정 변경

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -23,6 +23,8 @@ extra["springCloudVersion"] = "2024.0.1"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-config-server")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
